1c6ada0bff21069a57bfe42594e5fbd34dc923a3
oiskra <ojaskra@gmail.com>
2022-10-13 15:05:37 +0200

    image rendering

--- Projekt 2/script.js
+++ Projekt 2/script.js
@@ -1,2 +1,36 @@
-let slides = document.querySelector('#slides');
-console.log(slides.childNodes)
+const slides = document.querySelector('.slides');
+const dots = document.querySelectorAll('.dot');
+const prevButton = document.querySelector('#prevBtn');
+const nextButton = document.querySelector('#nextBtn');
+
+const images = [
+    'http://picsum.photos/seed/picsum/600/400',
+    'http://picsum.photos/seed/picsum/600/400',
+    'http://picsum.photos/seed/picsum/600/400',
+    'http://picsum.photos/seed/picsum/600/400',
+    'http://picsum.photos/seed/picsum/600/400',
+    'http://picsum.photos/seed/picsum/600/400'
+];
+
+const generateImages = () => {
+    images.forEach(image => {
+        const imgTag = document.createElement('img');
+        imgTag.src = image
+        slides.appendChild(imgTag)
+        console.log(imgTag)
+    })
+}
+
+prevButton.addEventListener('click', () => { 
+    
+})
+
+nextButton.addEventListener('click', () => { 
+    
+})
+
+
+
+
+
+generateImages();
