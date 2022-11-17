221066eb20fe9f134befa84e64922cb44f345466
oiskra <ojaskra@gmail.com>
2022-11-17 19:04:13 +0100

    added orientation event

--- Projekt 5/script.js
+++ Projekt 5/script.js
@@ -3,18 +3,49 @@ const width = window.innerWidth - 100;
 const height = window.innerHeight - 100
 let posX = 0
 let posY = 0
-let speedX = 5
-let speedY = 5
-
+let speedX = 0
+let speedY = 0
+let alphaMin, alphaMax;
+let betaMax, betaMin; 
+let baseAlpha = undefined
+let baseBeta = undefined
 
 const animateBallMovement = () => {
-    if(posX >= width || posX < 0) speedX *= (-1)
-    if(posY >= height || posY < 0) speedY *= (-1)
+    if(posX >= width || posX < 0) speedX = 0
+    if(posY >= height || posY < 0) speedY = 0
     
     posX += speedX
-    posY += speedY
+    //posY += speedY
     ball.style.left = posX + 'px'
-    ball.style.top = posY + 'px'
+    //ball.style.top = posY + 'px'
     requestAnimationFrame(animateBallMovement)
 }
 requestAnimationFrame(animateBallMovement)
+
+
+window.addEventListener('deviceorientation', e => {
+    if(baseAlpha === undefined && baseBeta === undefined) {
+        baseAlpha = e.alpha
+        baseBeta = e.beta
+
+        console.log('baseA',baseAlpha)
+        console.log('baseB',baseBeta)
+
+        alphaMax = baseAlpha + 90
+        alphaMin = baseAlpha - 90
+
+        betaMax = baseBeta + 90
+        betaMin = baseBeta - 90
+    }
+    speedX = speedX < 0 ? -5 : 5
+
+    // speedX = e.alpha > alphaMax ? 10 : 5  
+    // speedX = e.alpha < alphaMin ? 10 : 5  
+    
+    speedY = e.beta > betaMax ? 10 : e.beta/9  
+    speedY = e.beta < betaMin ? 10 : e.beta/9  
+
+    console.log('xspeed',speedX)
+    console.log('yspeed',speedY)
+
+})
