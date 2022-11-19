ad517856d267557f78d3578e72a46d3ce01d8c54
oiskra <ojaskra@gmail.com>
2022-11-19 01:04:39 +0100

    added vector struct

---
+++ Projekt 5/vector.js
@@ -0,0 +1,27 @@
+
+class Vector {
+    constructor(x,y) {
+        this.posX = x
+        this.posY = y
+    }
+
+    static sub(v1, v2) {
+        //standard substraction
+    }
+
+    static div(v1, v2) {
+        //standard division
+    }
+
+    mag() {
+        //a2+b2=c2
+        //from posx & posy
+        //calc c
+    }
+
+    setMag(num) {
+        //a2+b2=c2
+        //num = new c
+        //calc posX & poxY
+    }
+}
